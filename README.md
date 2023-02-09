6938517adab2bb1d701d7c6e18d17c577060008e
Sarah <sacred13_heart@yahoo.com>
2023-02-09 21:31:20 +0000

    added lab2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lab201--Lab-about-me
 Lab about me project- lab 2
+
+![Getting Started](screenshot.jpg)
---
+++ js/app.js
@@ -0,0 +1,65 @@
+'use strict';
+
+let username = prompt ('what is your name?');
+alert ('Hello '  +  username  + ' Welcome to my Site!');
+
+function getBoybands(){
+    let Boybands = prompt("Do I like Boybands?");
+    if (Boybands == " " || Boybands== null ||Boybands == undefined){
+    alert ("Try again! where are you going?");
+    getBoybands ();
+    } else {
+        alert ("Backstreet boys fan in the house!");
+    } 
+}
+getBoybands()
+
+let question2 = prompt ('Do you want ice cream?')
+console.log (question2);
+alert ('You said '+ question2)
+
+if (question2 === 'yes'){
+    prompt ('What kind of flavor?')
+    alert ('That is an awesome choice!')
+}
+
+if (question2 === 'no'){
+    prompt ('Are you kidding me!')
+    alert ('I will serve you ice cream for you to try')
+}
+
+let question3 = prompt ('Do you like walnuts on your ice cream?');
+console.log (question3);
+alert ('You said '+ question3)
+
+if (question3 === 'Yes'){
+    alert ('Me too, I love walnuts!')
+}
+
+if (question3 === 'No'){
+    alert ('I think you should try it!')
+}
+
+let question4 = prompt ('Do you like brownies with ice cream');
+console.log (question4);
+alert ('You said '+ question4)
+
+if (question4 === 'yes'){
+    alert ('It is the best mix right!')
+}
+
+if (question4 === 'no') {
+    alert (' That is okay, not all people do')
+}
+
+let question5 = prompt ('Do you like Oatly ice cream?');
+console.log (question5);
+alert ('You said '+ question5)
+
+if (question5 === 'yes'){
+    alert (' Me too, but it is expensive!')
+}
+
+if (question5 === 'no'){
+    alert (' It is fine, Oatly is a new trend')
+}
