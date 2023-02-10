921f916c942ff59e5027251f2327a4b13045e9d0
Sarah <sacred13_heart@yahoo.com>
2023-02-10 05:34:18 +0000

    modified js

--- js/app.js
+++ js/app.js
@@ -3,59 +3,44 @@
 let username = prompt ('what is your name?');
 alert ('Hello '  +  username  + ' Welcome to my Site!');
 
-let Boybands = prompt("Do I like Boybands?"); {
-    if (Boybands === 'y' || Boybands === 'n'){
+let Boybands = prompt ("Do I like Boybands?");{
+    if (Boybands === 'y' || Boybands === 'yes'){
         alert ("Backstreet boys fan in the house")
-     } else if (Boybands == 'yes' || Boybands == 'no')
-        alert ("Backstreet boys fan in the house!");
+     } else if (Boybands === 'n' || Boybands === 'no')
+        alert ("Boybands are here to stay!");
     } 
 
-let question2 = prompt ('Do you want ice cream?')
+let question2= prompt ('Do you want ice cream?');{
+if (question2 === 'y' || question2 === 'yes'){
+    alert ('Great, here is some ice cream')
+ } else if (question2 === 'n' || question2 === 'no')
+    alert ('I will serve you some anyhow!');
+} 
 console.log (question2);
 alert ('You said '+ question2)
 
-if (question2 === 'yes'){
-    prompt ('What kind of flavor?')
-    alert ('That is an awesome choice!')
-}
-
-if (question2 === 'no'){
-    prompt ('Are you kidding me!')
-    alert ('I will serve you some to try')
-}
-
-let question3 = prompt ('Do you like walnuts on your ice cream?').toUpperCase();
+let question3= prompt ('What kind of flavor?');{
+    if (question3 === 'y' || question3 === 'yes'){
+        alert ('That is an awesome choice!')
+     } else if (question2 === 'n' || question2 === 'no')
+        alert ('It is okay if you do not want any!');
+    } 
 console.log (question3);
 alert ('You said '+ question3)
 
-if (question3 === 'YES'){
-    alert ('Me too, I love walnuts!')
-}
+let question4 = prompt ('Do you like walnuts on your ice cream?').toUpperCase();
+    if (question4 === 'y' || question4 === 'yes'){
+        alert ('Me too, I love walnuts!')
+     } else if (question4 === 'n' || question4 === 'no')
+        alert ('Are you kidding me!');
 
-if (question3 === 'NO'){
-    alert ('I think you should try it!')
-}
+ console.log (question4);
 
-let question4 = prompt ('Do you like brownies with ice cream').toLowerCase();
-console.log (question4);
-alert ('You said '+ question4)
+ let question5 = prompt ('Do you like Oatly ice cream?');{
+ if (question5 === 'y' || question5 === 'yes'){
+     alert ('Me too, but it is expensive!')
+  } else if (question5 === 'n' || question5 === 'no')
+      alert ('It is okay, it is a new trend!')
+ }
 
-if (question4 === 'yes'){
-    alert ('It is the best mix right!')
-}
-
-if (question4 === 'no') {
-    alert (' That is okay, not all people do')
-}
-
-let question5 = prompt ('Do you like Oatly ice cream?');
 console.log (question5);
-alert ('You said '+ question5)
-
-if (question5 === 'yes'){
-    alert (' Me too, but it is expensive!')
-}
-
-if (question5 === 'no'){
-    alert (' It is fine, Oatly is a new trend')
-}
